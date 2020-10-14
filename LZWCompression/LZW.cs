46a8465ee9d7994d61fea5219e71c8fc59c2b486
Mariana <marianagonzalezlopez01@gmail.com>
2020-10-13 23:20:24 -0600

    Creación de biblioteca de clases

---
+++ LZWCompression/LZW.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LZWCompression
+{
+    class LZW
+    {
+        int id = 0;
+        Dictionary<int, byte> diccionario = new Dictionary<int, byte>();
+        public FileStream compression(string ruta, string nombre)
+        {
+            using var fileWritten = new FileStream(ruta, FileMode.OpenOrCreate);
+            using var reader = new BinaryReader(fileWritten);
+            var buffer = new byte[5000];
+            while (fileWritten.Position < fileWritten.Length)
+            {
+                buffer = reader.ReadBytes(5000);
+                hacerDiccionarioInicial(buffer);
+            }
+
+        }
+        public void hacerDiccionarioInicial (byte[] arr)
+        {
+            foreach (var caracter in arr)
+            {
+                if (!diccionario.ContainsValue(caracter)){
+                    id++;
+                    diccionario.Add(id, caracter);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        public void aumentarDiccionario(byte [] arr)
+        {
+            while (arr.Length > 0){
+                
+            }
+        }
+
+
+    }
+}
