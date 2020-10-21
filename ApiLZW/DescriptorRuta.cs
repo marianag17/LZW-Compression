ff326534f82fe1254e7454887ad3b1bff42fc973
JuanSolis <juansolisgtsk@hotmail.com>
2020-10-21 14:14:30 -0600

    Creacion de un descriptor con las rutas de la api en ruta '/'

---
+++ ApiLZW/DescriptorRuta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiLZW
+{
+    public class DescriptorRuta
+    {
+        public string Ruta { get; set; }
+        public string Descripcion { get; set; }
+        public string Metodo { get; set; }
+    }
+}
